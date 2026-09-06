2f7a17a3b9c69ed0a7dc118ccad502bdd0e1db94
OtakuTotipotent <otakutotipotent@gmail.com>
2026-09-06 21:38:27 +0500

    Revert "feat: add initial base tempate pages"
    This reverts commit 5b0a1d7d66b2045c10c8b14cfa129f2686165959.

--- client/src/pages/RegisterPage.jsx
+++
@@ -1,9 +0,0 @@
-const RegisterPage = () => {
-  return (
-    <main className="flex min-h-screen items-center justify-center px-6">
-      <h1 className="text-3xl font-semibold">Create account</h1>
-    </main>
-  );
-};
-
-export default RegisterPage;
---
+++ server/README.md
@@ -0,0 +1,17 @@
+# CoreClip
+
+`Generate professional Ads using AI`
+
+## Backend/Server Setup
+
+```cmd
+cd server
+npm install
+```
+
+## Tech Stack
+
+- Node.js
+- Express.js
+- mongoose
+- cloudinary
